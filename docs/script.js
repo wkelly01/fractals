0ccda0fcb231773d421c28ef35265436c83d1d37
wkelly01 <86485705+wkelly01@users.noreply.github.com>
2021-11-10 11:40:26 -0600

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -3,6 +3,8 @@ var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 var ctx = canvas.getContext("2d");
 
+
+
 function T1(point) {
     point[0] = 0.5 * point[0];
     point[1] = 0.5 * point[1];
@@ -16,25 +18,30 @@ function T3(point) {
     point[1] = (0.5 * point[1]) + (0.5 * point[2]);
 }
 
-
-for (j=0 ; j<50000 ; j++) {
-    point = [0.5 * canvasWidth, 0.5 * canvasWidth, canvasWidth]
-    for (i=0 ; i<=2000 ; i++) {
-        let x = Math.random()
-        switch (true) {
-            case x < 1/3:
-                T1(point);
-                break;
-            case x < 2/3:
-                T2(point);
-                break;
-            case x < 1:
-                T3(point);
-                break;
+function myFunction() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let val1Input = Number(document.getElementById("val1Input").value);
+    let val2Input = Number(document.getElementById("val2Input").value);
+    let val3Input = Number(document.getElementById("val3Input").value);
+    for (j=0 ; j<50000 ; j++) {
+        point = [0.5 * canvasWidth, 0.5 * canvasWidth, canvasWidth]
+        for (i=0 ; i<=2000 ; i++) {
+            let x = Math.random()
+            switch (true) {
+                case x < ((val1Input)/(val1Input + val2Input + val3Input)):
+                    T1(point);
+                    break;
+                case x < ((val1Input + val2Input)/(val1Input + val2Input + val3Input)):
+                    T2(point);
+                    break;
+                case x < 1:
+                    T3(point);
+                    break;
+            }
         }
-    }
-    ctx.beginPath();
-    ctx.arc(point[0], point[1], 0.1, 0, 2 * Math.PI);
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(point[0], point[1], 0.1, 0, 2 * Math.PI);
+        ctx.stroke();
 
+    }
 }
