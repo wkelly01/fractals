50614317590e720f44ef9046ffae5ad405e92744
wkelly01 <86485705+wkelly01@users.noreply.github.com>
2022-01-26 17:31:32 -0600

    Update script.js

--- RandomWheels/script.js
+++ RandomWheels/script.js
@@ -9,9 +9,9 @@ canvasGraph.width = window.innerHeight * 0.68;
 let pointsArrayX, pointsArrayY, distances //arrays to hold points and distances
 let fillColor //?? why do you exist
 let point //starting point
-let iterations = 100000 //Number of iterations of the random walks
+let iterations = 200 //Number of iterations of the random walks
 
-setInterval (simulate , 3000)
+setInterval (simulate , 1)
 //simulate()
 
 
