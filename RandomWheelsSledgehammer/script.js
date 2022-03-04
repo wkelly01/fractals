16c9f949a6ae0af45004db3b97a33b01528e0eb5
wkelly01 <86485705+wkelly01@users.noreply.github.com>
2022-03-04 10:28:42 -0600

    Update script.js

--- RandomWheelsSledgehammer/script.js
+++ RandomWheelsSledgehammer/script.js
@@ -1,7 +1,43 @@
+console.clear()
 //number of graphs to graph
 let c, ctx, l;
+//pointX[] = [x,y,currentDist,minDist,maxDist]
 let point = new Object()
 point.step0 = [[0.2,0.4]]
+for (i=2 ; i<=4 ; i++){
+    point.step0[0][i]=distanceForm(point.step0[0])
+}
+
+function doTheThing(n){
+    document.getElementById("header").remove()
+    for (i=1 ; i<=n ; i++){
+        eval("point.step"+i+"=new Array()")
+    }
+
+    for (j=1 ; j<=n ; j++){
+        eval("func1 ( point.step"+j+" , point.step"+(j-1)+")")
+    }
+
+    //axis window dimenstions 
+    eval("l = Math.ceil(maxValueXY(point.step"+n+"))")
+
+    for (i=0 ; i<=n ; i++) {
+        g = document.createElement('div');
+        g.style = "text-align: center"
+        document.body.appendChild(g)
+        h = document.createElement("canvas")
+        eval('h.id = "c'+i+'"')
+        g.appendChild(h)
+    }
+
+    for (k=0 ; k<=n ; k++) {
+        eval('axisMaker("c'+k+'")');
+        for (j=0 ; j<k ; j++) {
+            eval('func2(point.step'+j+',"gray")')
+        }
+        eval("func2(point.step"+k+")")
+    }
+}
 
 // My Functions
 function axisMaker(id){
@@ -27,11 +63,44 @@ function axisMaker(id){
 function func1 (arr, arrSeed) {
     for ( i=0 ; i < (3 * arrSeed.length) ; i+= 3){
     arr[i] = new Array()
-    T1(arrSeed[i/3][0],arrSeed[i/3][1],arr[i])
+        T1(arrSeed[i/3][0],arrSeed[i/3][1],arr[i])
+        arr[i][2]=distanceForm(arr[i])
+        if (arr[i][2] < arrSeed[i/3][3]) {
+            arr[i][3] = arr[i][2]
+        } else {
+            arr[i][3] = arrSeed[i/3][3]
+        }
+        if (arr[i][2] > arrSeed[i/3][4]) {
+            arr[i][4] = arr[i][2]
+        } else {
+            arr[i][4] = arrSeed[i/3][4]
+        }
     arr[i+1] = new Array()
-    T2(arrSeed[i/3][0],arrSeed[i/3][1],arr[i+1])
+        T2(arrSeed[i/3][0],arrSeed[i/3][1],arr[i+1])
+        arr[i+1][2]=distanceForm(arr[i+1])
+        if (arr[i+1][2] < arrSeed[i/3][3]) {
+            arr[i+1][3] = arr[i+1][2]
+        } else {
+            arr[i+1][3] = arrSeed[i/3][3]
+        }
+        if (arr[i+1][2] > arrSeed[i/3][4]) {
+            arr[i+1][4] = arr[i+1][2]
+        } else {
+            arr[i+1][4] = arrSeed[i/3][4]
+        }
     arr[i+2] = new Array()
-    T3(arrSeed[i/3][0],arrSeed[i/3][1],arr[i+2])
+        T3(arrSeed[i/3][0],arrSeed[i/3][1],arr[i+2])
+        arr[i+2][2]=distanceForm(arr[i+2])
+        if (arr[i+2][2] < arrSeed[i/3][3]) {
+            arr[i+2][3] = arr[i+2][2]
+        } else {
+            arr[i+2][3] = arrSeed[i/3][3]
+        }
+        if (arr[i+2][2] > arrSeed[i/3][4]) {
+            arr[i+2][4] = arr[i+2][2]
+        } else {
+            arr[i+2][4] = arrSeed[i/3][4]
+        }
     }
 }
 function func2(arr,color="black"){
@@ -78,32 +147,8 @@ function T3(x,y,arr) {
     arr[1] = ((Math.sin(Math.PI / 3) * x) + (Math.cos(Math.PI / 3) * y)) - (1/2);
 }
 //Transformation functions END
-function doTheThing(n){
-    for (i=1 ; i<=n ; i++){
-        eval("point.step"+i+"=new Array()")
-    }
 
-    for (j=1 ; j<=n ; j++){
-        eval("func1 ( point.step"+j+" , point.step"+(j-1)+")")
-    }
 
-    //axis window dimenstions 
-    eval("l = Math.ceil(maxValueXY(point.step"+n+"))")
-
-    for (i=0 ; i<=n ; i++) {
-        g = document.createElement('div');
-        g.style = "text-align: center"
-        document.body.appendChild(g)
-        h = document.createElement("canvas")
-        eval('h.id = "c'+i+'"')
-        g.appendChild(h)
-    }
-
-    for (k=0 ; k<=n ; k++) {
-        eval('axisMaker("c'+k+'")');
-        for (j=0 ; j<k ; j++) {
-            eval('func2(point.step'+j+',"gray")')
-        }
-        eval("func2(point.step"+k+")")
-    }
+function distanceForm(arr){
+    return Math.sqrt(Math.pow(arr[0],2) + Math.pow(arr[1],2))
 }
