44c1fd3fe63147ae780f87ca6771730e6b85e058
wkelly01 <86485705+wkelly01@users.noreply.github.com>
2022-04-08 14:20:41 -0500

    Update script.js

--- RandomWheelsSledgehammer/script.js
+++ RandomWheelsSledgehammer/script.js
@@ -1,16 +1,12 @@
 console.clear()
-let rotation, e_1, f_1, e_2, f_2;
-const colors = ["#2f4f4f","#2e8b57","#8b0000","#808000","#483d8b","#b03060","#ff0000","#ff8c00","#ffff00","#00ff00","#ba55d3","#00fa9a","#e9967a","#00ffff","#00bfff","#0000ff","#ff00ff","#f0e68c","#dda0dd","#2f4f4f","#2e8b57","#8b0000","#808000","#483d8b","#b03060","#ff0000","#ff8c00","#ffff00","#00ff00","#ba55d3","#00fa9a","#e9967a","#00ffff","#00bfff","#0000ff","#ff00ff","#f0e68c","#dda0dd","#2f4f4f","#2e8b57","#8b0000","#808000","#483d8b","#b03060","#ff0000","#ff8c00","#ffff00","#00ff00","#ba55d3","#00fa9a","#e9967a","#00ffff","#00bfff","#0000ff","#ff00ff","#f0e68c","#dda0dd"]
-
+let rotation, e_1, f_1, e_2, f_2, e_3, f_3;
+const colors = ["#2f4f4f","#2e8b57","#8b0000","#808000","#483d8b","#b03060","#ff0000","#ff8c00","#ffff00","#00ff00","#ba55d3","#00fa9a","#e9967a","#00ffff","#00bfff","#0000ff","#ff00ff","#f0e68c","#dda0dd"]
 let data = new Object();
 
-function doTheThing(n,rot,e1,f1,e2,f2,x,y){
-    setUsUpThePage(rot,e1,f1,e2,f2)
+function doTheThing(n,rot,e1,f1,e2,f2,e3,f3,x,y){
+    setUsUpThePage(rot,e1,f1,e2,f2,e3,f3)
     createStarterData(x,y);
-    createPathPoints(n);
-    analysisSteps(n);
-    uniquenessPrune(n);
-    uniquenessColors(n);
+    createPrunedPathPoints(n);
     createPageTable(n);
     graphTheGraphs(n);
     graphTheLegends(n);
@@ -18,7 +14,7 @@ function doTheThing(n,rot,e1,f1,e2,f2,x,y){
 
 //Main Functions
 //~~Set up the page with inputted values and cleare previous run
-function setUsUpThePage(rot,e1,f1,e2,f2){
+function setUsUpThePage(rot,e1,f1,e2,f2,e3,f3){
     if (document.getElementById("data") != null){
         document.getElementById("data").remove()
     }
@@ -30,47 +26,95 @@ function setUsUpThePage(rot,e1,f1,e2,f2){
     f_1 = f1;
     e_2 = e2;
     f_2 = f2;
+    e_3 = e3;
+    f_3 = f3;
 }
 //~~Set up initial Step for Data Object
 function createStarterData(x,y){
     data.analysis = new Object();
     data.step0 = new Object();
     data.step0.point1 = new point([x,y])
+    data.analysis.step0 = {MaxDistanceOfPath:data.step0.point1.MaxDistanceOfPath,
+                           MinDistanceOfPath:data.step0.point1.MinDistanceOfPath,
+                           GreatestUniqueCount:[1]
+                          }
 }
-//~~Create Points for each additional Step
-function createPathPoints(n){
-    for ( i=1 ; i<=n ; i++){
-        eval("data.step"+i+" = new Object()");
-        for ( j=1 ; j <= Math.pow(3,i) ; j++) {
-            eval("data.step"+i+".point"+j+" = new point(T"+(((j-1)%3)+1)+"(data.step"+(i-1)+".point"+(1+(Math.floor((j-1)/3)))+".coordinateRect))")
-            if (eval("data.step"+i+".point"+j+".MaxDistanceOfPath < data.step"+(i-1)+".point"+(1+(Math.floor((j-1)/3)))+".MaxDistanceOfPath")){
-                eval("data.step"+i+".point"+j+".MaxDistanceOfPath = data.step"+(i-1)+".point"+(1+(Math.floor((j-1)/3)))+".MaxDistanceOfPath")
+//~~New Function to create Pruned Path Points
+function createPrunedPathPoints(n){
+    for ( i=1 ; i <=n ; i++){
+        eval("data.step"+i+" = new Object()"); //Create empty nth step Object
+        let step
+            eval("step = data.step"+i)
+        let preStep
+            eval("preStep = data.step"+(i-1))
+        let stepAnal
+            eval("stepAnal = data.analysis.step"+i+" = new Object()")
+        let stepPAnal
+            eval("stepPAnal = data.analysis.step"+(i-1))
+        let previousArray = Object.keys(preStep)
+        eval("key = previousArray.length * 3")
+        for ( j=1 ; j <= key ; j++) {
+            eval("pointC = data.step"+i+".point"+j+"")
+            eval("T = T"+(((j-1)%3)+1))
+            eval("pointC = data.step"+i+".point"+j+" = new point(T(preStep[previousArray[Math.floor((j-1)/3)]].coordinateRect))") 
+            if (pointC.MaxDistanceOfPath < preStep[previousArray[Math.floor((j-1)/3)]].MaxDistanceOfPath){
+                pointC.MaxDistanceOfPath = preStep[previousArray[Math.floor((j-1)/3)]].MaxDistanceOfPath
             }
-            if (eval("data.step"+i+".point"+j+".MinDistanceOfPath > data.step"+(i-1)+".point"+(1+(Math.floor((j-1)/3)))+".MinDistanceOfPath")){
-                eval("data.step"+i+".point"+j+".MinDistanceOfPath = data.step"+(i-1)+".point"+(1+(Math.floor((j-1)/3)))+".MinDistanceOfPath")
+            if (pointC.MinDistanceOfPath > preStep[previousArray[Math.floor((j-1)/3)]].MinDistanceOfPath){
+                pointC.MinDistanceOfPath = preStep[previousArray[Math.floor((j-1)/3)]].MinDistanceOfPath
             }
+            pointC.UniquenessCounter = preStep[previousArray[Math.floor((j-1)/3)]].UniquenessCounter
+        }
+        
+        let currentArray = new Array()
+            eval("currentArray = Object.keys(data.step"+i+")")
+        analysisSteps(step,currentArray,stepAnal,stepPAnal)
+        uniquenessPrune(step,currentArray)
+            eval("currentArray = Object.keys(data.step"+i+")")
+        uniquenessColors(step,currentArray,stepAnal)
+    }
+}
+//~~Create an Object conting only Unique values 
+function uniquenessPrune(step,currentArray){
+    let epsilon = 0.0000002
+    for (ui = 0 ; ui < currentArray.length ; ui++){
+        if (step[currentArray[ui]] != undefined){
+            for (uj = ui+1 ; uj < currentArray.length ; uj++){
+                if (step[currentArray[uj]] != undefined){
+                    x1 = step[currentArray[ui]].coordinateRect[0]
+                    x2 = step[currentArray[uj]].coordinateRect[0]
+                    y1 = step[currentArray[ui]].coordinateRect[1]
+                    y2 = step[currentArray[uj]].coordinateRect[1]
+                    if ((Math.abs(x1 - x2) <= epsilon) && (Math.abs(y1 - y2) <= epsilon)) {
+                        step[currentArray[ui]].UniquenessCounter += step[currentArray[uj]].UniquenessCounter
+                        delete step[currentArray[uj]]
+                    }
+                }
+            }   
         }
     }
 }
 //~~Analysis setps of points per step
-function analysisSteps(n){
-    for (i=0 ; i<=n ; i++) {
-        eval("data.analysis.step"+i+" = new Object()")
-        var path = eval("data.step"+i)
-        eval("data.analysis.step"+i+".MaxDistanceOfPath = path.point1.MaxDistanceOfPath")
-        for (j=2 ; j<=Math.pow(3,i) ; j++){
-            if (eval("data.analysis.step"+i+".MaxDistanceOfPath < path.point"+j+".MaxDistanceOfPath")){
-                eval("data.analysis.step"+i+".MaxDistanceOfPath = path.point"+j+".MaxDistanceOfPath")
-            }
+function analysisSteps(step,currentArray,stepAnal,stepPAnal){
+    stepAnal.MaxDistanceOfPath = stepPAnal.MaxDistanceOfPath;
+    stepAnal.MinDistanceOfPath = stepPAnal.MinDistanceOfPath;
+    for (ui=0 ; ui < currentArray.length ; ui++){
+        if (step[currentArray[ui]].MaxDistanceOfPath > stepAnal.MaxDistanceOfPath){
+            stepAnal.MaxDistanceOfPath = step[currentArray[ui]].MaxDistanceOfPath
         }
-        eval("data.analysis.step"+i+".MinDistanceOfPath = path.point1.MinDistanceOfPath")
-        for (j=2 ; j<=Math.pow(3,i) ; j++){
-            if (eval("data.analysis.step"+i+".MinDistanceOfPath > path.point"+j+".MinDistanceOfPath")){
-                eval("data.analysis.step"+i+".MinDistanceOfPath = path.point"+j+".MinDistanceOfPath")
-            }
+        if (step[currentArray[ui]].MinDistanceOfPath < stepAnal.MinDistanceOfPath){
+            stepAnal.MinDistanceOfPath = step[currentArray[ui]].MinDistanceOfPath
         }
-        eval("data.analysis.step"+i+".GreatestUniqueCount = []")
     }
+    stepAnal.GreatestUniqueCount = []
+}
+//~~Create any array of values correlating to Uniqueness counts
+function uniquenessColors(step,currentArray,stepAnal){
+    let stepAUC = stepAnal.GreatestUniqueCount
+    for (ui = 0 ; ui < currentArray.length ; ui++){
+        stepAUC.push(step[currentArray[ui]].UniquenessCounter)
+    }
+    uniqSort(stepAUC)
 }
 //~~Create the table to display the graph and information
 function createPageTable(n){
@@ -115,41 +159,6 @@ function createPageTable(n){
         }
     }
 }
-//~~Create an Object conting only Unique values 
-function uniquenessPrune(n){
-    epsilon = 0.0000002
-    for (i = 1 ; i <= n ; i++) {
-        eval("a = Object.keys(data.step"+i+").length")
-        for ( j=1 ; j <= a ; j++) {
-            if (eval("data.step"+i+".point"+j+" != undefined")){
-                for (k=j+1 ; k <=a ; k++){
-                    if (eval("data.step"+i+".point"+k+" != undefined")){
-                        eval("x1 = data.step"+i+".point"+j+".coordinateRect[0]")
-                        eval("x2 = data.step"+i+".point"+k+".coordinateRect[0]")
-                        eval("y1 = data.step"+i+".point"+j+".coordinateRect[1]")
-                        eval("y2 = data.step"+i+".point"+k+".coordinateRect[1]")
-                        if ((Math.abs(x1 - x2) <= epsilon) && (Math.abs(y1 - y2) <= epsilon)) {
-                            eval("delete data.step"+i+".point"+k+"");
-                            eval("data.step"+i+".point"+j+".UniquenessCounter++")
-                            
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-//~~Create any array of values correlating to Uniqueness counts
-function uniquenessColors(n){
-    for (i=0 ; i<=n ; i++){
-        for (j=1 ; j <= Math.pow(3,i) ; j++){
-            if(eval("data.step"+i+".point"+j+" != undefined")){
-                eval("data.analysis.step"+i+".GreatestUniqueCount.push(data.step"+i+".point"+j+".UniquenessCounter)")
-            }
-        }
-        eval("uniqSort(data.analysis.step"+i+".GreatestUniqueCount)")
-    }
-}
 //~~Create the Axis and Grpah all points to hte Axis
 function graphTheGraphs(n){
     for (i=0 ; i<=n ; i++){
@@ -178,27 +187,31 @@ function graphTheGraphs(n){
             a = 0
         }
         for (j=a ; j<=i ; j++) {
-            a = Math.pow(3,j)
-            for (k=1 ; k <= a ; k++){
-                if (eval("data.step"+j+".point"+k+" != undefined")){
-                    eval("x = data.step"+j+".point"+k+".coordinateRect[0];")
-                    eval("y = data.step"+j+".point"+k+".coordinateRect[1];")
-                    if (j == i){
-                        eval("a1 = data.analysis.step"+j+".GreatestUniqueCount")
-                        eval("a2 = data.step"+j+".point"+k+".UniquenessCounter")
-                        a3 = a1.indexOf(a2)
-                        color = colors[a3]
-                    }else {
-                        color = "#D3D3D3"
-                    }
-                    ctx.save();
-                    ctx.fillStyle = color
-                    ctx.beginPath();
-                    ctx.arc(Px(x), Py(y), 2, 0, 2 * Math.PI);
-                    ctx.stroke();
-                    ctx.fill()
-                    ctx.restore();
+            let step
+                eval("step = data.step"+j)
+            let currentArray = Object.keys(step)
+            let stepAnal
+                eval("stepAnal = data.analysis.step"+j)
+            for (k=0 ; k < currentArray.length ; k++){
+                x = step[currentArray[k]].coordinateRect[0];
+                y = step[currentArray[k]].coordinateRect[1];
+                if (j == i){
+                    a1 = stepAnal.GreatestUniqueCount
+                    console.log(a1)
+                    a2 = step[currentArray[k]].UniquenessCounter
+                    console.log(a2)
+                    a3 = a1.indexOf(a2)
+                    color = colors[a3]
+                }else {
+                    color = "#D3D3D3"
                 }
+                ctx.save();
+                ctx.fillStyle = color
+                ctx.beginPath();
+                ctx.arc(Px(x), Py(y), 2, 0, 2 * Math.PI);
+                ctx.stroke();
+                ctx.fill()
+                ctx.restore();
             }
         }
     }
@@ -206,22 +219,38 @@ function graphTheGraphs(n){
 //~~Create the Legends
 function graphTheLegends(n){
     for (i=0 ; i<=n ; i++){
+        let step
+        eval("step = data.step"+i)
+        let currentArray = Object.keys(step)
+        let stepAnal
+        eval("stepAnal = data.analysis.step"+i)
+        let analArray = Object.keys(stepAnal.GreatestUniqueCount)
         c= document.getElementById("l"+i);
         ctx = c.getContext("2d");
         c.width = window.innerHeight * 0.50
-        c.height= window.innerHeight * 0.05
-        eval("la = data.analysis.step"+i+".GreatestUniqueCount")
+        rowHeight = window.innerHeight * 0.05
+        c.height= rowHeight * Math.ceil(analArray.length / 8)
+        console.log(c.height)
+        rowHeight = window.innerHeight * 0.05
+        let la = stepAnal.GreatestUniqueCount
         for (j=0 ; j<la.length ; j++){
-            ctx.fillStyle = colors[j]
-            ctx.fillRect(0 + ((j*c.width)/la.length) , 0, (1/la.length) * c.width, 0.5*c.height)
+            ctx.fillStyle = colors[j%colors.length]
+            ca = (1/8)*c.width * (j  % 8)
+            cb = Math.floor(j/8) * rowHeight
+            cc = (1/8)*c.width
+            cd = 0.5*rowHeight
+            ctx.fillRect(ca, cb, cc, cd)
             ctx.font = "16px Arial";
             ctx.textAlign = "center";
             ctx.fillStyle = "black"
-                x = ((1/2) * (1/la.length) * c.width) + ((j*c.width)/la.length)
-            ctx.fillText(la[j], x, c.height);
+                x = ca + (1/2)*cc
+                y = rowHeight + (rowHeight * Math.floor(j/8)) -2
+                //y = c.height
+            ctx.fillText(la[j], x, y);
         }
     }
 }
+
 //Math Functionss
 //~~Distance formula (From Center)~~
 function distanceFormula(arr){
@@ -252,19 +281,20 @@ function Py(y){
     return -(c.height * 0.5 * y / l) + (c.height * 0.5)
 }
 
+//Transformation Functions
 function T1(arr) {
-    let x = strip(((Math.cos(rotation) * arr[0]) - (Math.sin(rotation) * arr[1])));
-    let y = strip(((Math.sin(rotation) * arr[0]) + (Math.cos(rotation) * arr[1])));
-    return [x,y]
-}
-function T2(arr) {
     let x = strip(((Math.cos(rotation) * arr[0]) - (Math.sin(rotation) * arr[1])) + (e_1));
     let y = strip(((Math.sin(rotation) * arr[0]) + (Math.cos(rotation) * arr[1])) + (f_1));
     return [x,y]
 }
-function T3(arr) {
+function T2(arr) {
     let x = strip(((Math.cos(rotation) * arr[0]) - (Math.sin(rotation) * arr[1])) + (e_2));
     let y = strip(((Math.sin(rotation) * arr[0]) + (Math.cos(rotation) * arr[1])) + (f_2));
+    return [x,y]
+}
+function T3(arr) {
+    let x = strip(((Math.cos(rotation) * arr[0]) - (Math.sin(rotation) * arr[1])) + (e_3));
+    let y = strip(((Math.sin(rotation) * arr[0]) + (Math.cos(rotation) * arr[1])) + (f_3));
     return [x,y]
 }
 
